c61b14f358b93b4ab7b961b46d6a94df5208829c
Elena55555 <kastalskayaelena@gmail.ru>
2024-12-29 09:33:52 +0300

    HW_Kotlin

---
+++ Kotlin/src/main/kotlin/com/example/Kotlin/KotlinApplication.kt
@@ -0,0 +1,17 @@
+package com.example.Kotlin
+
+fun main( ) {
+	val age: Int
+	age = 33
+
+	val name: String
+	name = "Elena"
+
+	val isStudent: Boolean
+	isStudent = true
+
+	val salary: Double
+	salary = 500000.0
+
+	println("Age: " + age + " |"  + " Name: " + name + " |" + " Is Student: " + isStudent + " |"  + " Salary: " + salary)
+}
